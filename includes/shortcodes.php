f84a4da7b2ae36a580c9bed52e08a5613e9ca67e
dtw <dtw@users.github.com>
2019-06-15 21:14:41 +0100

    Fix class names

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1,6 +1,6 @@
 <?php // signposts shortcode - lists categories for signposts
 
-function signposts_list_categories_with_featured_images() {
+function signposts_list-categories_with_featured_images() {
 // List LOCAL SERVICES with link to each
   $terms = get_terms( 'signpost_categories' );
   if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
@@ -9,8 +9,8 @@ function signposts_list_categories_with_featured_images() {
       $the_term_icon = get_term_meta( $term->term_id, 'icon', true );
       $stuff = $stuff .
       '<div class="col-md-3">
-        <div class="signpost_container">
-          <img class="img_signpost_category" src="' . $the_term_icon . '" alt="' . $term->name . '" />
+        <div class="signpost-container">
+          <img class="img-signpost-category" src="' . $the_term_icon . '" alt="' . $term->name . '" />
           <div class="signpost-details">
             <p>
               <a class="signpost-title ' . $term->slug . '" href="' . get_term_link( $term ) . '">' . $term->name . '</a>
