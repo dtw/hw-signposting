1883944092fe738118cbfba98b9c8547e3587b9e
dtw <dtw@users.github.com>
2019-12-22 17:53:28 +0000

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -4,7 +4,7 @@ function signposts_list_categories_with_featured_images() {
 // List LOCAL SERVICES with link to each
   $terms = get_terms( 'signpost_categories' );
   if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
-    $output = '<div id="signposts-selector" class="row">';
+    $output = '<!-- start container --><div id="signposts-selector" class="row">';
     foreach ( $terms as $term ) {
       $the_term_icon = get_term_meta( $term->term_id, 'icon', true );
       $output = $output .
@@ -22,7 +22,7 @@ function signposts_list_categories_with_featured_images() {
         </div>
       </div>';
     }
-    $output = $output . '</div>';
+    $output = $output . '</div><!-- end of container -->';
   }
 return $output;
 }
