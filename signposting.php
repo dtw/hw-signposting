169884efecf1b0a5e658765093fa5e64fbc22a18
dtw <dtw@users.github.com>
2020-01-03 17:25:03 +0000

    Comments and whitespace

--- signposting.php
+++ signposting.php
@@ -60,16 +60,6 @@ function hw_signpost() {
   );
 }
 
-
-
-
-
-
-
-
-
-
-
 /* 2. Register CUSTOM TAXONOMY
 (Note that must add taxonomy to custom post type too)
 		- signpost categories
@@ -98,6 +88,19 @@ function signposts_categories_init() {
 }
 add_action( 'init', 'signposts_categories_init' );
 
+/* 3. Add Admin COLUMNS
+------------------------------------------------------------------ */
+
+/**
+This code modified from "Show modified Date in admin lists" plugin
+Plugin URI: http://apasionados.es
+Description: Shows a new, sortable, column with the modified date in the lists of pages and posts in the WordPress admin panel. It also shows the username that did the last update.
+Version: 1.1
+Author: Apasionados.es
+Author URI: http://apasionados.es
+License: GPL2
+*/
+
 // Register Modified Date and Last Author Column for signposts post_type
 function modified_column_register( $columns ) {
   $columns['LastAuthor'] = __( 'Last Modified by', 'hw_signposting_show_modified_date_in_admin_lists' );
@@ -109,32 +112,34 @@ add_filter( 'manage_signposts_posts_columns', 'modified_column_register' );
 // Populate the two columns
 function modified_column_display( $column_name, $post_id ) {
 	switch ( $column_name ) {
-	case 'Modified':
+	// show modified date (with timestamp on hover)
+  case 'Modified':
 		global $post;
 	       	echo '<p class="mod-date">';
           echo '<span style="text-decoration: dotted underline;" title="'.get_the_modified_date().' '.get_the_modified_time().'">';
 	       	echo get_the_modified_date($d='Y/m/d').'</span>';
 			echo '</p>';
-    break; // end all case breaks
+    break;
+  // show last author
   case 'LastAuthor':
   	global $post;
          	echo '<p class="mod-author">';
           echo get_the_modified_author();
   		echo '</p>';
-		break; // end all case breaks
+		break;
 	}
 }
 add_action( 'manage_signposts_posts_custom_column', 'modified_column_display', 10, 2 );
 
 // Make the Modified column sortable
-
 function modified_column_register_sortable( $columns ) {
 	$columns['Modified'] = 'modified';
 	return $columns;
 }
 add_filter( 'manage_edit-signposts_sortable_columns', 'modified_column_register_sortable' );
 
-// adds the signposting shortcodes to a new signpost automatically
+/* 4. Add the signposting shortcodes to a new signpost automatically
+------------------------------------------------------------------ */
 
 function signposting_editor_content( $content, $post ) {
 
@@ -156,11 +161,13 @@ function signposting_editor_content( $content, $post ) {
 
 add_filter( 'default_content', 'signposting_editor_content', 10, 2 );
 
+/* 5. Other signposts post type admin
+------------------------------------------------------------------ */
+
 function my_remove_wp_seo_meta_box() {
 	remove_meta_box('wpseo_meta', 'signposts', 'normal');
 }
 add_action('add_meta_boxes', 'my_remove_wp_seo_meta_box', 100);
-
 
 function addtoany_disable_sharing_on_my_custom_post_type() {
     if ( 'signposts' == get_post_type() ) {
@@ -169,17 +176,7 @@ function addtoany_disable_sharing_on_my_custom_post_type() {
 }
 add_filter( 'addtoany_sharing_disabled', 'addtoany_disable_sharing_on_my_custom_post_type' );
 
-
-
-
-
-
-
-
-
 include("includes/taxonomy-icons.php");
 include("includes/shortcodes.php");
-
-
 
 ?>
