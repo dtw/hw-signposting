4b0da21d03a24d6b455d2f1c2a1606766f00247c
Phil T <362017+dtw@users.noreply.github.com>
2022-06-06 12:24:24 +0100

    Update capabilities for taxonomy
    Basically permissions

--- signposting.php
+++ signposting.php
@@ -46,7 +46,12 @@ function hw_signpost_create_post_type() {
 		'public' => true,
 		'has_archive' => true,
 		'exclude_from_search' => false,
-		'capability_type' => 'post',
+    'capabilities' => array (
+      'manage_terms' => 'activate_plugins',
+      'edit_terms' => 'activate_plugins',
+      'delete_terms' => 'activate_plugins',
+      'assign_terms' => 'post',
+    ),
 		'show_ui' => true,
 		'menu_position' => 4,
 		'show_in_nav_menus' => false,
